242c16d9e3e96b60b5d681b9e078848cf49ccbe3
pyavchik <pyavchik@gmail.com>
2018-06-26 13:18:48 +0300

    BaseTest.java change @BeforeTest annotation with @BeforeClass added @Optional("firefox") added profiles to pom.xml

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,24 +2,23 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Created by pyavchik on 26.06.2018.
- */
+
 public class BaseTest {
 
     WebDriver driver;
 
-    @Parameters("browser")
-    @BeforeTest
-    public void beforeTest(String browser) throws MalformedURLException {
-
+    @Parameters({"browser"})
+    @BeforeClass(alwaysRun = true)
+    public void beforeTest(@Optional("firefox") String browser) throws MalformedURLException {
 
         DesiredCapabilities cap;
 
@@ -34,7 +33,7 @@ public class BaseTest {
         } else if (browser.equals("safari")){
             cap = DesiredCapabilities.safari();
             cap.setBrowserName("safari");
-            cap.setPlatform(Platform.ANY);
+            cap.setPlatform(Platform.MAC);
         } else {
             cap = DesiredCapabilities.firefox();
             cap.setBrowserName("firefox");
@@ -42,9 +41,12 @@ public class BaseTest {
         }
 
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @AfterTest
+    @AfterClass
     public void afterTest(){
         driver.quit();
     }
